8b0e2aec7ae2137572f905b827e1f9d89fcaf177
Dfen90 <dfenchik@gmail.com>
2024-08-09 16:43:38 +0300

    Update

--- src/games/even.js
+++ src/games/even.js
@@ -1,4 +1,5 @@
-import { getRandomInt, runGame } from '../index.js';
+import { getRandomInt } from '../utils.js';
+import { runGame } from '../index.js';
 
 const isEven = (num) => {
   const result = (num % 2 === 0);
