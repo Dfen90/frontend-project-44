e29d6b8350f3da8a4b3cee4aa8656a9696cd7d02
Dfen90 <dfenchik@gmail.com>
2024-08-09 20:30:24 +0300

    +

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,33 +1,32 @@
 import { getRandomInt } from '../utils.js';
 import { runGame } from '../index.js';
 
-const generateProgression = (length, start, step) => {
-    return Array.from({ length }, (_, i) => start + i * step);
+const generateProgression = (length, start, step) =>
+  Array.from({ length }, (_, i) => start + i * step);
+  
+const hideElement = (progression, index) => {
+  const newProgression = [...progression];
+  newProgression[index] = '..';
+  return newProgression;
+};
+  
+const generateQuestionAndAnswer = () => {
+  const progressionLength = getRandomInt(5, 10); 
+  const start = getRandomInt(1, 20); 
+  const step = getRandomInt(1, 10); 
+  const progression = generateProgression(progressionLength, start, step);
+  const hiddenIndex = getRandomInt(0, progressionLength - 1);
+  const correctAnswer = progression[hiddenIndex].toString();
+  const questionProgression = hideElement(progression, hiddenIndex);
+  
+  const question = `Question: ${ questionProgression.join(' ') }`;
+  return [question, correctAnswer];
   };
   
-  const hideElement = (progression, index) => {
-    const newProgression = [...progression];
-    newProgression[index] = '..';
-    return newProgression;
+const runProgressionGame = () => {
+  const rulesMessage = 'What number is missing in the progression?';
+  runGame(rulesMessage, generateQuestionAndAnswer);
   };
   
-  const generateQuestionAndAnswer = () => {
-    const progressionLength = getRandomInt(5, 10); 
-    const start = getRandomInt(1, 20); 
-    const step = getRandomInt(1, 10); 
-    const progression = generateProgression(progressionLength, start, step);
-    const hiddenIndex = getRandomInt(0, progressionLength - 1);
-    const correctAnswer = progression[hiddenIndex].toString();
-    const questionProgression = hideElement(progression, hiddenIndex);
-  
-    const question = `Question: ${ questionProgression.join(' ') }`;
-    return [question, correctAnswer];
-  };
-  
-  const runProgressionGame = () => {
-    const rulesMessage = 'What number is missing in the progression?';
-    runGame(rulesMessage, generateQuestionAndAnswer);
-  };
-  
-  export default runProgressionGame;
+export default runProgressionGame;
   
