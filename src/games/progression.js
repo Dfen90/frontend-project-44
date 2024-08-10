9d5a3e4b5dc411010df122b99941f5e86d613b03
Dfen90 <dfenchik@gmail.com>
2024-08-10 16:47:29 +0300

    Update

--- src/games/progression.js
+++ src/games/progression.js
@@ -5,7 +5,7 @@ const generateProgression = (length, start, step) => {
   const progression = [];
   for (let i = 0; i < length; i += 1) {
     progression.push(start + i * step);
-  };
+  }
   return progression;
 };
 
